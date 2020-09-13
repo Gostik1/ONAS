fb208128e6382165ffd60a82d0c6ece0137a21de
Gostik1 <71230379+Gostik1@users.noreply.github.com>
2020-09-13 22:33:24 +0300

    Add files via upload

---
+++ pract1_Poplavskiy_Alexsandr.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+import pandas as pd
+
+
+# In[3]:
+
+
+pd.read_csv
+
+
+# In[4]:
+
+
+pd.r
+
+
+# In[5]:
+
+
+pd.
+
+
+# In[6]:
+
+
+help(len)
+
+
+# In[7]:
+
+
+import numpy as np
+
+
+# In[8]:
+
+
+get_ipython().run_line_magic('pinfo', 'np.rank')
+
+
+# In[9]:
+
+
+def square(a):
+    "Return the square of a"
+    return a ** 2
+
+
+# In[10]:
+
+
+get_ipython().run_line_magic('pinfo2', 'square')
+
+
+# In[ ]:
+
+
+
+
